3f47167fe5fbb83375c2d208715d3dec7bcf5cc2
Revan <60571063+aklerza@users.noreply.github.com>
2022-12-06 16:37:19 +0400

    Update 4.py

--- 3-hefte/4.py
+++ 3-hefte/4.py
@@ -3,7 +3,7 @@ m=[]
 while n > 0:
     m.append(n%10)
     n//=10
-m.sort(reverse=True)
+#reverse() istifadə etmədim. çünki ədədlər onsuz da soldan sağa əlavə edilir yuxarıdakı dövrün nəticəsinə əsasən.
 eded=0
 for i in range(0,len(m)):
     eded+=m[i]*(10**(len(m)-1-i))
